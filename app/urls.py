8074fa4b892d3477df24b911f1ff18b2cab8d993
alex <alex@example.com>
2018-03-21 11:26:40 +0100

    add basic views and urls

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('main/', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('search/', views.search, name='search'),
+    path('contact/', views.contact, name='contact'),
+    
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
 # Create your views here.
 
@@ -12,6 +13,21 @@ But do not worry, in a few Sprints, it will be both functional, and
 beautiful! See You soon ~~
 """
 
+about_us = """
+The web portal "Lucky Paw" was established by group of students
+in order to help homeless pets to find their new home
+There are still more information to add ...
+(⌐ ͡■ ͜ʖ ͡■) 
+"""
 
 def index(request):
-    return HttpResponse(to_be_continued)
+    return render(request, 'app/index.html')
+
+def about(request):
+    return render(request, 'app/about.html')
+
+def search(request):
+    return render(request, 'app/search.html')
+
+def contact(request):
+    return render(request, 'app/contact.html')
