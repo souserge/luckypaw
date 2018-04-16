978947a580c2f0cfbdd97cfa2015f9fd36952d50
Stalex89 <alex@example.com>
2018-04-16 17:50:48 +0200

    add more filters, model update

--- app/filters.py
+++ app/filters.py
@@ -10,10 +10,20 @@ from .models import Pet
 
 class PetFilter(django_filters.FilterSet):
 
-    # pet_name = django_filters.CharFilter(name='pet_name', lookup_expr='icontains')
-    # pet_type = django_filters.MultipleChoiceFilter(choices=Pet.pet_type_choice, widget=forms.CheckboxSelectMultiple)
-    
+    #pet_name = django_filters.CharFilter(name='pet_name', lookup_expr='icontains')
+    #pet_type = django_filters.MultipleChoiceFilter(choices=Pet.pet_type_choice, widget=forms.CheckboxSelectMultiple)
+    #pet_age = django_filters.MultipleChoiceFilter(choices=Pet.pet_age_choice, widget=forms.CheckboxSelectMultiple)
+    #pet_colour = django_filters.CharFilter(name='pet_color', lookup_expr='icontains')
+    #pet_gender = django_filters.MultipleChoiceFilter(choices=Pet.pet_gender_choice, widget=forms.CheckboxSelectMultiple)
+    #pet_size = django_filters.MultipleChoiceFilter(choices=Pet.pet_size_choice, widget=forms.CheckboxSelectMultiple)
+    #pet_breed = django_filters.CharFilter(name='pet_breed', lookup_expr='icontains')
+    #pet_spayed = django_filters.BooleanFilter(widget=forms.CheckboxInput)
+    #pet_vaccinated = django_filters.BooleanFilter(widget=forms.CheckboxInput)
+    #pet_housetrained = django_filters.BooleanFilter(widget=forms.CheckboxInput)
+    #pet_specialcare = django_filters.BooleanFilter(widget=forms.CheckboxInput)
+
     class Meta:
         model = Pet
-        fields = ['pet_type', 'pet_location']
+        fields = ['pet_type', 'pet_location','pet_age','pet_color','pet_gender',
+        'pet_spayed','pet_vaccinated','pet_housetrained','pet_specialcare','pet_size','pet_breed']
         
--- app/models.py
+++ app/models.py
@@ -14,11 +14,15 @@ class Pet(models.Model):
     pet_color = models.CharField(max_length=50, default='', blank=True)
     pet_gender_choice = (('Male','Male'),('Female','Female'))
     pet_gender = models.CharField(max_length=50, choices=pet_gender_choice, blank=True)
-    pet_size_choice = (('XSmall','XSmall'), ('Small','Small'),('Medium','Medium'),('Large','Large'),('XLarge','XLarge'))
+    pet_size_choice = (('Very Small','Very Small'), ('Small','Small'),('Medium','Medium'),('Large','Large'),('Very Large','Very Large'))
     pet_size = models.CharField(max_length=50, choices=pet_size_choice, blank=True)
     pet_breed = models.CharField(max_length=50, default='', blank=True)
     #pet_needs_choice = (('Spayed/Neutered','Spayed/Neutered'),('Vaccinated','Vaccinated'),('Purebred','Purebred'),('House-trained','House-trained'))
-    
-        
+    pet_spayed = models.BooleanField()
+    pet_vaccinated = models.BooleanField()
+    pet_housetrained = models.BooleanField()
+    pet_specialcare = models.BooleanField()
+
+
     def __str__(self):
         return self.pet_name
