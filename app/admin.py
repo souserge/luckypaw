d3fe12ec8056f29fbe00d86c810b567fcf525909
Stalex89 <23526237+Stalex89@users.noreply.github.com>
2018-04-28 22:29:17 +0200

    add SuperAdmin and PetAdmin for admin model browsing

--- app/admin.py
+++ app/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 from .models import Supervisor, Pet
 
 # Register your models here.
-admin.site.register(Supervisor)
-admin.site.register(Pet)
+
+class SuperAdmin(admin.ModelAdmin):
+    list_display = ["user", "city", "country", "telephone"]
+
+    class Meta:
+        model = Supervisor
+
+class PetAdmin(admin.ModelAdmin):
+    list_display = ["name", "animaltype", "location", "age", "color", "gender", "size", "breed",
+        "spayed", "vaccinated", "housetrained", "specialcare", "gallery", "photo"]
+
+    class Meta:
+        model = Pet
+
+admin.site.register(Supervisor, SuperAdmin)
+admin.site.register(Pet, PetAdmin)
