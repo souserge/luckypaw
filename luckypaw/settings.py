ef3179aad69a48ad4c43f46e0de086c0eabeb687
souserge <15277406+souserge@users.noreply.github.com>
2018-05-31 17:23:47 +0200

    Add supervisor when new user is added

--- luckypaw/settings.py
+++ luckypaw/settings.py
@@ -166,3 +166,52 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_KEY ='1007848219210-ot8r82d9tv9g4hjs7dp4n9hhhfca877l.a
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'TLBuRC-ug9YgCb8vPhkcUEZl' #Paste Secret Key
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+SOCIAL_AUTH_PIPELINE = (
+    # Get the information we can about the user and return it in a simple
+    # format to create the user instance later. On some cases the details are
+    # already part of the auth response from the provider, but sometimes this
+    # could hit a provider API.
+    'social_core.pipeline.social_auth.social_details',
+
+    # Get the social uid from whichever service we're authing thru. The uid is
+    # the unique identifier of the given user in the provider.
+    'social_core.pipeline.social_auth.social_uid',
+
+    # Verifies that the current auth process is valid within the current
+    # project, this is where emails and domains whitelists are applied (if
+    # defined).
+    'social_core.pipeline.social_auth.auth_allowed',
+
+    # Checks if the current social-account is already associated in the site.
+    'social_core.pipeline.social_auth.social_user',
+
+    # Make up a username for this person, appends a random string at the end if
+    # there's any collision.
+    'social_core.pipeline.user.get_username',
+
+    # Send a validation email to the user to verify its email address.
+    # Disabled by default.
+    # 'social_core.pipeline.mail.mail_validation',
+
+    # Associates the current social details with another user account with
+    # a similar email address. Disabled by default.
+    # 'social_core.pipeline.social_auth.associate_by_email',
+
+    # Create a user account if we haven't found one yet.
+    'social_core.pipeline.user.create_user',
+
+    # Create the record that associates the social account with the user.
+    'social_core.pipeline.social_auth.associate_user',
+
+    # Populate the extra_data field in the social record with the values
+    # specified by settings (and the default ones like access_token, etc).
+    'social_core.pipeline.social_auth.load_extra_data',
+
+    # Update the user record with any changed info from the auth service.
+    'social_core.pipeline.user.user_details',
+
+    # Create the record that associates the user with the supervisor
+    'app.aux.associate_user_with_sup',
+)
